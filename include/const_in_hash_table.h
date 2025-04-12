225d0fddd85f268bcfb9f4a3294513a533fdf8a6
Alexandr26Starostin <sanchostarostin@gmail.com>
2025-04-12 17:52:49 +0300

    version 0: prepare for hash_table

---
+++ include/const_in_hash_table.h
@@ -0,0 +1,9 @@
+#ifndef CONST_IN_HASH_TABLE_H
+#define CONST_IN_HASH_TABLE_H
+
+enum errors_in_hash_table_t
+{
+	NOT_ERROR = 0
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <const_in_hash_table.h>
+
+int main ()
+{
+	printf ("Hello, I'm hash_table\n");
+
+	return NOT_ERROR;
+}
